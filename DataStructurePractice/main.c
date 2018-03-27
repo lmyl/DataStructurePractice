503db560bd664069dba352080a39b3c0e185d0b6
LmLy <1269458422@qq.com>
2018-03-27 12:52:13 +0800

    Initial Commit

---
+++ DataStructurePractice/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  DataStructurePractice
+//
+//  Created by ly on 2018/3/27.
+//  Copyright © 2018年 LY. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
