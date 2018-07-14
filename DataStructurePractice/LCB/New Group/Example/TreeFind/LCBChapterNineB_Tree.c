4101fde3fb4a5b5578bd28e4a6ad03bc6c0de8bd
LmLy <1269458422@qq.com>
2018-07-14 19:41:25 +0800

    书中第九章程序

--- DataStructurePractice/LCB/New Group/Example/TreeFind/LCBChapterNineB_Tree.c
+++ DataStructurePractice/LCB/New Group/Example/TreeFind/LCBChapterNineB_Tree.c
@@ -7,3 +7,30 @@
 //
 
 #include "LCBChapterNineB_Tree.h"
+
+int findLocationInBTNode(BTNode *node , KeyType key)  ;
+
+B_TreeResult b_TreeSearch(BTNode *tree,KeyType key) {
+    B_TreeResult result  ;
+    if (tree == NULL) {
+        result.tag = 2;
+        return result ;
+    }
+    int location = findLocationInBTNode(tree, key) ;
+    if (location < tree->keyNum && tree->key[location] == key) {
+        result.index = location ;
+        result.tag = 1 ;
+        result.result = tree ;
+        return result ;
+    }
+    return b_TreeSearch(tree->child[location], key);
+}
+
+int findLocationInBTNode(BTNode *node , KeyType key) {
+    for (int i = 0; i<node->keyNum; i++) {
+        if (key <= node->key[i]) {
+            return i ;
+        }
+    }
+    return node->keyNum ;
+}
--- DataStructurePractice/LCB/New Group/Example/TreeFind/LCBChapterNineB_Tree.h
+++ DataStructurePractice/LCB/New Group/Example/TreeFind/LCBChapterNineB_Tree.h
@@ -10,5 +10,26 @@
 #define LCBChapterNineB_Tree_h
 
 #include <stdio.h>
+#define MAXM 10
+
+typedef int KeyType;
+typedef struct btreenode {
+    int keyNum ;    //节点拥有关键字类型
+    KeyType key[MAXM] ;
+    struct btreenode * parent ;
+    struct btreenode * child[MAXM] ;
+}BTNode;
+
+int stratum = 10 ;
+int MAXLength = 9 ;
+int MINLegnth = 4 ;
+
+typedef struct {
+    BTNode * result ;
+    int index ;  //在节点中的关键字序号
+    int tag ; //1:查找成功，2:查找失败
+}B_TreeResult;
+
+B_TreeResult b_TreeSearch(BTNode *tree,KeyType key) ;
 
 #endif /* LCBChapterNineB_Tree_h */
